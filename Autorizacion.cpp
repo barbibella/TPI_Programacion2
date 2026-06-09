94ced3658f0c82abec3c9e54a5dab426c9a19e8f
Thomas <151042162+Thomasbudeguer@users.noreply.github.com>
2026-06-09 17:20:03 -0300

    Autorizacion .h y .cpp

---
+++ Autorizacion.cpp
@@ -0,0 +1,143 @@
+#include <iostream>
+#include <cstring>
+#include<Autorizacion.h>
+using namespace std;
+
+
+
+///////Cons
+
+Autorizacion::Autorizacion(){
+
+    idOrden = 0;
+    codAutorizacion = 0;
+    porcentajeCobertura = 0;
+
+    aprobada = false;
+
+    strcpy(observaciones, "");
+}
+
+
+
+
+///////Gets
+
+int Autorizacion::getIdOrden(){
+    return idOrden;
+}
+
+int Autorizacion::getCodAutorizacion(){
+    return codAutorizacion;
+}
+
+Fecha Autorizacion::getFechaAutorizacion(){
+    return fechaAutorizacion;
+}
+
+float Autorizacion::getPorcentajeCobertura(){
+    return porcentajeCobertura;
+}
+
+bool Autorizacion::getAprobada(){
+    return aprobada;
+}
+
+const char* Autorizacion::getObservaciones(){
+    return observaciones;
+}
+
+
+/////sets
+
+
+void Autorizacion::setIdOrden(int id){
+    idOrden = id;
+}
+
+
+
+void Autorizacion::setCodAutorizacion(int cod){
+    codAutorizacion = cod;
+}
+
+
+void Autorizacion::setFechaAutorizacion(Fecha fecha){
+    fechaAutorizacion = fecha;
+}
+
+void Autorizacion::setPorcentajeCobertura(float porcentaje){
+    porcentajeCobertura = porcentaje;
+}
+
+void Autorizacion::setAprobada(bool estado){
+    aprobada = estado;
+}
+
+void Autorizacion::setObservaciones(const char* obs){
+    strcpy(observaciones, obs);
+}
+
+
+/////metodos (cargar y mostrar)
+
+void Autorizacion::Cargar(){
+
+    cout << "INGRESE EL ID DE LA ORDEN: ";
+    cin >> idOrden;
+    cout << endl;
+
+    cout << "INGRESE EL COD DE AUTORIZACIÓN";
+    cin >> codAutorizacion;
+
+    cout << "INGRESE EL PORCENTAJE DE COBER";
+    cin >> porcentajeCobertura;
+
+    cout << "ESTA APROBADA? (1 si o 0 no ): ";
+    cin >> aprobada;
+
+    cin.ignore();
+
+    cout << "Ingrese las observaciones (100 caract): ";
+    cin.getline(observaciones, 100);
+
+    cout << "Fecha de autorizacion:" << endl;
+    fechaAutorizacion.Cargar();
+}
+
+void Autorizacion::Mostrar(){
+
+    cout << "ID DE LA ORDEN: " << idOrden << endl;
+    cout << "EL CODIGO DE AUTORIZACION: " << codAutorizacion << endl;
+
+    cout << "FECHA DE AUTORIZACION : ";
+    fechaAutorizacion.Mostrar();
+
+    cout << endl;
+
+    cout << "PORCENTAJE DE COB: "
+         << porcentajeCobertura << "%" << endl;
+
+    cout << "ESTADO DE APROBACION: ";
+
+    if(aprobada){
+        cout << "SI";
+    }
+    else{
+        cout << "NO";
+    }
+
+    cout << endl;
+
+    cout << "OBSERVACIONES: " << observaciones << endl;
+}
+
+
+
+
+
+
+
+
+
+
---
+++ Autorizacion.h
@@ -0,0 +1,51 @@
+#pragma once
+#include "fecha.h"
+
+
+class Autorizacion {
+
+/////atrib
+
+private:
+
+    int idOrden;
+    int codAutorizacion;
+    Fecha fechaAutorizacion;
+    float porcentajeCobertura;
+    bool aprobada;
+    char observaciones[100];
+
+    //////Cons,met
+
+public:
+    Autorizacion();
+    int getIdOrden();
+    int getCodAutorizacion();
+    Fecha getFechaAutorizacion();
+    float getPorcentajeCobertura();
+    bool getAprobada();
+    const char* getObservaciones();
+
+    void setIdOrden(int id);
+    void setCodAutorizacion(int cod);
+    void setFechaAutorizacion(Fecha fecha);
+    void setPorcentajeCobertura(float porcentaje);
+    void setAprobada(bool estado);
+    void setObservaciones(const char* obs);
+
+
+    void Mostrar();
+    void Cargar();
+
+
+
+
+
+
+
+
+
+
+
+
+}
