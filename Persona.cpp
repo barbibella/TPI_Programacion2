ee42c47af3206cd25b7c86e6d4533b5ce9281805
MaiMayer <mariainesmayer@gmail.com>
2026-06-21 00:47:48 -0300

    Cambios Persona - no modificar

--- Persona.cpp
+++ Persona.cpp
@@ -3,45 +3,61 @@
 #include "Persona.h"
 
 using namespace std;
+// ===MAI===============================================================
+Persona::Persona()
+: _nombre(""),_apellido(""), _dni(""), _telefono(""), _estado(){}
 
-Persona::Persona() {
-    strcpy(_nombre, "");
-    strcpy(_apellido, "");
-    strcpy(_dni, "");
-    strcpy(_telefono, "");
-    _estado = false;
+//CONSTRUCTOR - Mai
+Persona::Persona(string nombre, string apellido, string dni, string telefono, bool estado){
+    setNombre(nombre);
+    setApellido (apellido);
+    setDni (dni);
+    setTelefono (telefono);
+    setEstado (estado);
+
+    }
+
+//SETTERS - Mai
+void Persona::setNombre(string nombre){
+    if (nombre.size()<30){
+        strcpy(_nombre, nombre.c_str());
+    }
+    else { strcpy(_nombre, "nombre fuera de rango");
+    }
 }
 
-Persona::Persona(string nombre, string apellido, string dni, string telefono, bool estado) {
-    strcpy(_nombre, nombre.c_str());
-    strcpy(_apellido, apellido.c_str());
-    strcpy(_dni, dni.c_str());
-    strcpy(_telefono, telefono.c_str());
+void Persona::setApellido(string apellido){
+    if(apellido.size()<30){
+            strcpy(_apellido, apellido.c_str());
+    }
+    else{ strcpy(_apellido, "apellido fuera de rango");
+    }
+}
+
+void Persona::setDni(string dni){
+    if (dni.size()<15){
+        strcpy(_dni, dni.c_str());
+    }
+    else { strcpy(_dni, "DNI fuera de rango");
+    }
+}
+
+void Persona::setTelefono(string telefono){
+    if (telefono.size()<20){
+        strcpy(_telefono, telefono.c_str());
+    }
+    else { strcpy(_telefono, "telefono fuera de rango");
+    }
+}
+
+void Persona::setEstado(bool estado){
     _estado = estado;
-}
+    }
 
-void Persona::setNombre(string nombre) {
-    strcpy(_nombre, nombre.c_str());
-}
 
-void Persona::setApellido(string apellido) {
-    strcpy(_apellido, apellido.c_str());
-}
-
-void Persona::setDni(string dni) {
-    strcpy(_dni, dni.c_str());
-}
-
-void Persona::setTelefono(string telefono) {
-    strcpy(_telefono, telefono.c_str());
-}
-
-void Persona::setEstado(bool estado) {
-    _estado = estado;
-}
-
-string Persona::getNombre() const {return _nombre; }
-string Persona::getApellido() const {return _apellido; }
-string Persona::getDni() const {return _dni; }
-string Persona::getTelefono() const {return _telefono; }
-bool Persona::getEstado() const {return _estado; }
+//GETTERS - Mai
+string Persona::getNombre() const { return _nombre;}
+string Persona::getApellido() const { return _apellido;}
+string Persona::getDni() const { return _dni;}
+string Persona::getTelefono() const { return _telefono;}
+bool Persona::getEstado() const { return _estado;}
