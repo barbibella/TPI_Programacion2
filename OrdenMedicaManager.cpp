4e089261145aa75812581acf457b7f3887ae1f48
Evelyn Olmedo <122215333+Eveolmedo@users.noreply.github.com>
2026-06-21 14:43:07 -0300

    Se agregan 2 nuevos listados
    En Orden Medica se agrega listar inactivos y activos

--- OrdenMedicaManager.cpp
+++ OrdenMedicaManager.cpp
@@ -12,31 +12,43 @@ void OrdenMedicaManager::menu(){
     do{
         system("cls");
         cout << "===== ORDENES MEDICAS =====" << endl;
-        cout << "1. LISTAR TODAS LAS ORDENES" << endl;
-        cout << "2. AGREGAR UNA ORDEN" << endl;
-        cout << "3. BUSCAR POR ID UNA ORDEN" << endl;
-        cout << "4. ELIMINAR UNA ORDEN" << endl;
-        cout << "5. REACTIVAR UNA ORDEN" << endl;
-        cout << "0. SALIR" << endl;
+        cout << "1. AGREGAR UNA ORDEN" << endl;
+        cout << "2. LISTAR TODOS" << endl;
+        cout << "3. LISTAR ACTIVOS" << endl;
+        cout << "4. LISTAR INACTIVOS" << endl;
+        cout << "5. BUSCAR POR ID" << endl;
+        cout << "6. MODIFICAR ORDEN MEDICA" << endl;
+        cout << "7. ELIMINAR UNA ORDEN" << endl;
+        cout << "8. REACTIVAR UNA ORDEN" << endl;
+        cout << "-------------------------" << endl;
+        cout << "0. VOLVER AL MENU PRINCIPAL" << endl;
         cout << "OPCION: ";
-
         cin >> opcion;
         system("cls");
 
         switch(opcion){
             case 1:
-                listarTodos();
-                break;
-            case 2:
                 agregar();
                 break;
+            case 2:
+                listarTodos();
+                break;
             case 3:
-                buscaPorID();
+                listarActivos();
                 break;
             case 4:
-                eliminar();
+                listarInactivos();
                 break;
             case 5:
+                buscaPorID();
+                break;
+            case 6:
+                modificar();
+                break;
+            case 7:
+                eliminar();
+                break;
+            case 8:
                 reactivar();
                 break;
             case 0:
@@ -48,22 +60,6 @@ void OrdenMedicaManager::menu(){
                 break;
         }
     }while(opcion != 0);
-}
-
-void OrdenMedicaManager::listarTodos(){
-    OrdenMedicaArchivo archivo;
-
-    int cantidad = archivo.contarRegistros();
-
-    for(int i = 0; i < cantidad; i++){
-        OrdenMedica reg;
-
-        reg = archivo.leer(i);
-
-        if(reg.getEstado()){
-            reg.mostrar();
-        }
-    }
 }
 
 void OrdenMedicaManager::agregar(){
@@ -80,6 +76,57 @@ void OrdenMedicaManager::agregar(){
     }
 
 
+}
+
+void OrdenMedicaManager::listarTodos(){
+    OrdenMedicaArchivo archivo;
+
+    int cantidad = archivo.contarRegistros();
+
+    for(int i = 0; i < cantidad; i++){
+        OrdenMedica reg;
+
+        reg = archivo.leer(i);
+        reg.mostrar();
+    }
+
+    system("pause");
+}
+
+void OrdenMedicaManager::listarActivos(){
+    OrdenMedicaArchivo archivo;
+
+    int cantidad = archivo.contarRegistros();
+
+    for(int i = 0; i < cantidad; i++){
+        OrdenMedica reg;
+
+        reg = archivo.leer(i);
+
+        if(reg.getEstado()){
+            reg.mostrar();
+        }
+    }
+
+    system("pause");
+}
+
+void OrdenMedicaManager::listarInactivos(){
+    OrdenMedicaArchivo archivo;
+
+    int cantidad = archivo.contarRegistros();
+
+    for(int i=0; i < cantidad; i++){
+        OrdenMedica reg;
+
+        reg = archivo.leer(i);
+
+        if(!reg.getEstado()){
+            reg.mostrar();
+        }
+    }
+
+    system("pause");
 }
 
 int OrdenMedicaManager::generarId(){
