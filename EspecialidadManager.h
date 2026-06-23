754f698f806084b3346a1cef52e7b969cff58622
MaiMayer <144709607+MaiMayer@users.noreply.github.com>
2026-06-23 13:23:23 -0300

    Actualizacion y desarrollo clases y funciones

---
+++ EspecialidadManager.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "Especialidad.h"
+#include "EspecialidadArchivo.h"
+#include <cstring>
+
+//--- MAI ---
+
+class EspecialidadManager
+{
+public:
+    EspecialidadManager();
+
+    void guardarEspecialidad();//crear
+    void listarEspecialidad();// LISTAR
+    void modificarEspecialidad();
+    void informacionEspecialidad();
+    Especialidad cargar();
+
+private:
+    Especialidad crearEspecialidad();
+    void mostrarEspecialidad(const Especialidad &reg);
+    EspecialidadArchivo _repoEspecialidad;
+};
