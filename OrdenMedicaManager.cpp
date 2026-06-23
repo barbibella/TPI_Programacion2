a55e24fb6ed89f2749d765199d235ec68f031a20
Evelyn Olmedo <122215333+Eveolmedo@users.noreply.github.com>
2026-06-23 14:55:01 -0300

    Se agregan listados en Orden Medica

--- OrdenMedicaManager.cpp
+++ OrdenMedicaManager.cpp
@@ -3,6 +3,8 @@
 #include "OrdenMedica.h"
 #include "OrdenMedicaArchivo.h"
 #include "OrdenMedicaManager.h"
+#include "TipoDeOrdenArchivo.h"
+#include "OrigenDeLaOrdenArchivo.h"
 
 using namespace std;
 
@@ -13,13 +15,12 @@ void OrdenMedicaManager::menu(){
         system("cls");
         cout << "===== ORDENES MEDICAS =====" << endl;
         cout << "1. AGREGAR UNA ORDEN" << endl;
-        cout << "2. LISTAR TODOS" << endl;
-        cout << "3. LISTAR ACTIVOS" << endl;
-        cout << "4. LISTAR INACTIVOS" << endl;
-        cout << "5. BUSCAR POR ID" << endl;
-        cout << "6. MODIFICAR ORDEN MEDICA" << endl;
-        cout << "7. ELIMINAR UNA ORDEN" << endl;
-        cout << "8. REACTIVAR UNA ORDEN" << endl;
+        cout << "2. LISTADOS" << endl;
+        cout << "3. BUSCAR POR ID" << endl;
+        cout << "4. MODIFICAR ORDEN MEDICA" << endl;
+        cout << "5. ELIMINAR UNA ORDEN" << endl;
+        cout << "6. REACTIVAR UNA ORDEN" << endl;
+        cout << "7. CONSULTAS" << endl;
         cout << "-------------------------" << endl;
         cout << "0. VOLVER AL MENU PRINCIPAL" << endl;
         cout << "OPCION: ";
@@ -31,25 +32,22 @@ void OrdenMedicaManager::menu(){
                 agregar();
                 break;
             case 2:
-                listarTodos();
+                menuListados();
                 break;
             case 3:
-                listarActivos();
-                break;
-            case 4:
-                listarInactivos();
-                break;
-            case 5:
                 buscaPorID();
                 break;
-            //case 6:
+            //case 4:
                 //modificar();
                // break;
-            case 7:
+            case 5:
                 eliminar();
                 break;
-            case 8:
+            case 6:
                 reactivar();
+                break;
+            case 7:
+                menuConsultas();
                 break;
             case 0:
                 cout << "Saliendo.." << endl;
@@ -70,6 +68,7 @@ void OrdenMedicaManager::agregar(){
 
     if(reg.cargar() == false){
         cout << "No se cargo la orden medica." << endl;
+        system("pause");
         return;
     }
 
@@ -78,6 +77,55 @@ void OrdenMedicaManager::agregar(){
     } else {
         cout << "Error al guardar la orden" << endl;
     }
+}
+
+void OrdenMedicaManager::menuListados(){
+    int opcion;
+
+    do{
+        system("cls");
+        cout << "===== LISTADOS =====" << endl;
+        cout << "1. LISTAR TODOS" << endl;
+        cout << "2. LISTAR ACTIVOS" << endl;
+        cout << "3. LISTAR INACTIVOS" << endl;
+        cout << "4. LISTAR ORDENADOS POR FECHA" << endl;
+        cout << "5. LISTAR ORDENADOS POR TIPO DE ORDEN" << endl;
+        cout << "6. LISTAR ORDENADOS POR ORIGEN DE LA ORDEN" << endl;
+        cout << "7. CONSULTAS" << endl;
+        cout << "-------------------------" << endl;
+        cout << "0. VOLVER AL MENU ANTERIOR" << endl;
+        cout << "OPCION: ";
+        cin >> opcion;
+        system("cls");
+
+        switch(opcion){
+            case 1:
+                listarTodos();
+                break;
+            case 2:
+                listarActivos();
+                break;
+            case 3:
+                listarInactivos();
+                break;
+            case 4:
+                listarPorFecha();
+                break;
+            case 5:
+                listarPorTipoOrden();
+               break;
+            case 6:
+               listarPorOrigen();
+               break;
+            case 0:
+                cout << "Saliendo.." << endl;
+                break;
+            default:
+                cout << "OPCION INCORRECTA" << endl;
+                system("pause");
+                break;
+        }
+    }while(opcion != 0);
 }
 
 void OrdenMedicaManager::listarTodos(){
@@ -125,6 +173,127 @@ void OrdenMedicaManager::listarInactivos(){
 
         if(!reg.getEstado()){
             reg.mostrar();
+        }
+    }
+
+    system("pause");
+}
+
+void OrdenMedicaManager::listarPorFecha(){
+    OrdenMedicaArchivo archivo;
+
+    int cantidad = archivo.contarRegistros();
+
+    if(cantidad == 0){
+        cout << "No hay ordenes cargadas." << endl;
+        system("pause");
+        return;
+    }
+
+    OrdenMedica *vec = new OrdenMedica[cantidad];
+
+    for(int i = 0; i < cantidad; i++){
+        vec[i] = archivo.leer(i);
+    }
+
+    for(int i = 0; i < cantidad - 1; i++){
+        for(int j = i + 1; j < cantidad; j++){
+
+            Fecha fechaI = vec[i].getFechaOrden();
+            Fecha fechaJ = vec[j].getFechaOrden();
+
+            if(fechaJ.getAnio() < fechaI.getAnio() ||
+              (fechaJ.getAnio() == fechaI.getAnio() &&
+               fechaJ.getMes() < fechaI.getMes()) ||
+              (fechaJ.getAnio() == fechaI.getAnio() &&
+               fechaJ.getMes() == fechaI.getMes() &&
+               fechaJ.getDia() < fechaI.getDia())){
+
+                OrdenMedica aux = vec[i];
+                vec[i] = vec[j];
+                vec[j] = aux;
+            }
+        }
+    }
+
+    for(int i = 0; i < cantidad; i++){
+        if(vec[i].getEstado()){
+            vec[i].mostrar();
+        }
+    }
+
+    delete[] vec;
+
+    system("pause");
+}
+
+void OrdenMedicaManager::listarPorTipoOrden(){
+    OrdenMedicaArchivo archivoOrdenes;
+    TipoDeOrdenArchivo archivoTipos;
+
+    int cantidadOrdenes = archivoOrdenes.contarRegistros();
+    int cantidadTipos = archivoTipos.contarRegistros();
+
+    for(int i=0; i < cantidadTipos; i++){
+
+        TipoDeOrden tipo;
+
+        tipo = archivoTipos.leer(i);
+
+        cout << endl;
+        cout << "===== TIPO DE ORDEN: " << tipo.getIdTipoOrden() << " - " << tipo.getDescripcion() << " =====" << endl;
+
+        bool encontro = false;
+
+        for(int j = 0; j < cantidadOrdenes; j++){
+
+            OrdenMedica reg = archivoOrdenes.leer(j);
+
+            if(reg.getEstado() && reg.getTipoOrden() == tipo.getIdTipoOrden()){
+                reg.mostrar();
+                encontro = true;
+            }
+        }
+
+        if(!encontro){
+            cout << "No hay ordenes de este tipo." << endl;
+        }
+    }
+
+    system("pause");
+}
+
+void OrdenMedicaManager::listarPorOrigen(){
+    OrdenMedicaArchivo archivoOrdenes;
+    OrigenDeLaOrdenArchivo archivoOrigen;
+
+    int cantidadOrdenes = archivoOrdenes.contarRegistros();
+    int cantidadOrigen = archivoOrigen.contarRegistros();
+
+
+    for(int i=0; i < cantidadOrigen; i++){
+        OrigenDeLaOrden origen;
+
+        origen = archivoOrigen.leer(i);
+
+        cout << endl;
+        cout << "===== ORIGEN DE LA ORDEN MEDICA: " << origen.getIdOrigen() << " - " << origen.getDescripcion() << " =====" << endl;
+
+        bool encontro = false;
+
+        for(int j = 0; j < cantidadOrdenes; j++){
+
+            OrdenMedica reg;
+            reg = archivoOrdenes.leer(j);
+
+            if(reg.getEstado() && reg.getOrigen() == origen.getIdOrigen()){
+                reg.mostrar();
+                encontro = true;
+            }
+        }
+
+        if(!encontro){
+            cout << "No hay ordenes de este con ese origen." << endl;
         }
     }
 
--- OrdenMedicaManager.h
+++ OrdenMedicaManager.h
@@ -7,14 +7,31 @@ class OrdenMedicaManager{
 
     public:
         void menu();
+
+        // Submenus
+        void menuListados();
+        void menuConsultas();
+
+        // ABM
         void agregar();
-        void listarTodos();
-        void listarActivos();
-        void listarInactivos();
-        void buscaPorID();
         void modificar();
         void eliminar();
         void reactivar();
+
+        // Listados
+        void listarTodos();
+        void listarActivos();
+        void listarInactivos();
+        void listarPorFecha();
+        void listarPorTipoOrden();
+        void listarPorOrigen();
+
+        // Busquedas y consultas
+        void buscaPorID();
+        void consultarPorDni();
+        void consultarPorMedico();
+        void consultarPorFecha();
+        void consultarPorTipoOrden();
 };
 
 
