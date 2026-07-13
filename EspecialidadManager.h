d6c5334dd74c33f3e2b928a64e888edd96abaccc
MaiMayer <144709607+MaiMayer@users.noreply.github.com>
2026-07-13 13:48:00 -0300

    Actualización y avance clase EspecialidadManager

--- EspecialidadManager.h
+++ EspecialidadManager.h
@@ -10,17 +10,19 @@ class EspecialidadManager
 public:
     EspecialidadManager();
 
-    void Menu();//FALTA AGREGAR URGENTE
-
-    void guardarEspecialidad();//crear
-    void listarEspecialidad();// LISTAR
-    void modificarEspecialidad();
-    void informacionEspecialidad(); //falta
-    void eliminarEspecialidad();    // puse la firma, falta el desarrollo en el cpp
     Especialidad cargar();
+    void guardarEspecialidad();//crear
+    void listarEspecialidad();/// LISTAR TODOS
+    void eliminarEspecialidad();  ///listo!
+    void altaEspecialidad(); /// LISTO!
+    void listarEspActivos(); /// LISTO!
+    bool listarEspInactivos(); ///LISTO!
+    void modificarEspecialidad(); ///FALTA!
+    void informacionEspecialidad(); /// FALTA!
+
 
 private:
     Especialidad crearEspecialidad();
-    void mostrarEspecialidad(const Especialidad &reg);
+    void mostrarEspecialidad(const Especialidad &reg);///Busca la informacion de 1 especialidad y la muestra
     EspecialidadArchivo _repoEspecialidad;
 };
